0aa03717917d346ada5d08b2c5e3c528632859d9
neriosoft <alabaneri@gmail.com>
2022-08-18 07:53:02 +0100

    adding knex query 1

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,18 @@
 let users = require('./users.json')
-
+const knex = require('./knex');
 const { writeDataToFile } = require('../utils')
 
 function findAll() {
+
+    knex.from('users').select('*').then(users => console.log(JSON.stringify(users)));
     return new Promise((resolve, reject) => {
+        
         resolve(users)
     })
 }
 
 function findById(id) {
+    knex.from('users').select('*').where('user.id', '=',id).then(user => console.log(JSON.stringify(user)));
     return new Promise((resolve, reject) => {
         const user = users.find((p) => p.id === id)
         resolve(user)
@@ -16,6 +20,9 @@ function findById(id) {
 }
 
 function create(user) {
+    knex('user').insert({ name: user.name, email: user.email, password: user.password}).then(() => 
+    console.log('user created successfully'))
+    
     return new Promise((resolve, reject) => {
         const newUser = {id: uuidv4(), ...user}
         users.push(newUser)
